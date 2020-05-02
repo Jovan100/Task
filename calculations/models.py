c6a3276b058fda12894ffe6a8e9f9fd7471d543d
JovanKroon <jovan@kroonstudio.com>
2020-05-02 12:29:59 +0200

    Calculations model created

--- calculations/models.py
+++ calculations/models.py
@@ -1,3 +1,8 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+class Calculations(models.Model):
+    numbers      = ArrayField(models.IntegerField(null=True, blank=True), null=True, blank=True)
+    calculations = ArrayField(models.IntegerField(null=True, blank=True), null=True, blank=True)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
