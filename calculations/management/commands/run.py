0d28aab14dac4908983a3cd51b706a5e68d3bb58
JovanKroon <jovan@kroonstudio.com>
2020-05-05 16:21:33 +0200

    Custom commands

---
+++ calculations/management/commands/run.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+import os
+
+class Command(BaseCommand):
+    help = "Runs server. If parameters are passed it runs commands: -mm -> makemigrations, -m -> migrate,\
+            -csu -> createsuperuser, -ir install requirements."
+
+    def add_arguments(self, parser):
+        parser.add_argument('-arg', '--arguments', type=str, nargs='*')
+
+    def handle(self, *args, **options):
+        arguments = options['arguments']
+        try:
+            if 'mm' in arguments:
+                call_command('makemigrations')
+            if 'm' in arguments:
+                call_command('migrate')
+            if 'csu' in arguments:
+                call_command('createsuperuser')
+            if 'ir' in arguments:
+                os.system('pip install -r requirements.txt')
+        except:
+            call_command('runserver', '127.0.0.1:7373')
