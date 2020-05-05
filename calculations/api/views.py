f73a84ac484008518c5eb1605d45d229d54201bc
JovanKroon <jovan@kroonstudio.com>
2020-05-05 13:02:01 +0200

    Views fixes

--- calculations/api/views.py
+++ calculations/api/views.py
@@ -3,26 +3,28 @@ from calculations.api.serializers import CalculationsSerializer
 from calculations.models import Calculations
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework import status
+
 class AddView(ViewSet):
+
     def add(self, request, numbers):
         if isinstance(request.data['num'], int):
             numbers.append(request.data['num'])
         elif isinstance(request.data['num'], str):
-            numbers.extend(list(map(int, request.data['num'].split(','))))
-        else:
-            numbers.extend(request.data['num'])
+            nums = [int(x) for x in request.data['num'].split(',')]
+            numbers.extend(nums)
         request.session['numbers'] = numbers
 
     def create(self, request, *args, **kwargs):
         try:
             numbers = request.session['numbers']
             self.add(request, numbers)
-        except (KeyError, AttributeError):
+        except KeyError:
             numbers = []
             self.add(request, numbers)
 
-        return Response(status=201)
+        return Response(data=request.session['numbers'], status=status.HTTP_201_CREATED)
 
 
 class CalculateView(ViewSet):
@@ -32,25 +34,25 @@ class CalculateView(ViewSet):
             calculations = request.session['calculations']
             calculations.append(calc)
             request.session['calculations'] = calculations
-            return Response(data=calc, status=201)
+            return Response(data=calc, status=status.HTTP_201_CREATED)
         except KeyError:
             try:
                 calc = sum(request.session['numbers'])
                 request.session['calculations'] = [calc]
-                return Response(data=calc, status=201)
-            except:
+                return Response(data=calc, status=status.HTTP_201_CREATED)
+            except KeyError:
                 message = 'There are no numbers.'
-                return Response(data=message, status=404)
+                return Response(data=message, status=status.HTTP_404_NOT_FOUND)
 
     def list(self, request, all=None, **kwargs):
         if all == 'all':
             try:
-                return Response(data=request.session['calculations'], status=200)
+                return Response(data=request.session['calculations'], status=status.HTTP_200_OK)
             except KeyError:
-                message = 'There aro no calculations.'
-                return Response(data=message, status=404)
+                message = 'There are no calculations.'
+                return Response(data=message, status=status.HTTP_404_NOT_FOUND)
         else:
-            return Response(status=400)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 class ResetView(ViewSet):
     def create(self, request, *args, **kwargs):
@@ -66,26 +68,28 @@ class ResetView(ViewSet):
                 serialized.save()
                 del request.session['numbers']
                 del request.session['calculations']
-                return Response(data=serialized.data, status=201)
+                return Response(data=serialized.data, status=status.HTTP_201_CREATED)
             else:
-                return Response(status=400)
+                return Response(status=status.HTTP_400_BAD_REQUEST)
         except KeyError:
             message = 'There are no numbers to add.'
-            return Response(data=message, status=400)
+            return Response(data=message, status=status.HTTP_400_BAD_REQUEST)
 
 class HistoryView(ViewSet):
     def get_permissions(self):
         if self.action == 'list':
             permission_classes = [IsAdminUser]
+        if self.action == 'retrieve':
+            permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
 
     def retrieve(self, request, pk=None, **kwargs):
         queryset = Calculations.objects.all()
         history = get_object_or_404(queryset, pk=pk)
         serialized = CalculationsSerializer(history)
-        return Response(serialized.data, status=200)
+        return Response(serialized.data, status=status.HTTP_200_OK)
 
     def list(self, request, *args, **kwargs):
         queryset = Calculations.objects.all()
         serialized = CalculationsSerializer(queryset, many=True)
-        return Response(serialized.data, status=200)
+        return Response(serialized.data, status=status.HTTP_200_OK)
