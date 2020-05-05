53afe24537d9be801014db200f11c92edfdcc090
JovanKroon <jovan@kroonstudio.com>
2020-05-05 16:32:17 +0200

    Command for testing added

--- calculations/management/commands/run.py
+++ calculations/management/commands/run.py
@@ -3,8 +3,8 @@ from django.core.management import call_command
 import os
 
 class Command(BaseCommand):
-    help = "Runs server. If parameters are passed it runs commands: -mm -> makemigrations, -m -> migrate,\
-            -csu -> createsuperuser, -ir install requirements."
+    help = "Runs server. If parameters are passed it runs commands: mm -> makemigrations, m -> migrate,\
+            csu -> createsuperuser, ir -> install requirements, t -> test."
 
     def add_arguments(self, parser):
         parser.add_argument('-arg', '--arguments', type=str, nargs='*')
@@ -18,6 +18,8 @@ class Command(BaseCommand):
                 call_command('migrate')
             if 'csu' in arguments:
                 call_command('createsuperuser')
+            if 't' in arguments:
+                call_command('test')
             if 'ir' in arguments:
                 os.system('pip install -r requirements.txt')
         except:
